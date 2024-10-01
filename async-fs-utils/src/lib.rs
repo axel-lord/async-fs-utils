ce65c80865572c3d09dbefe91676bc646d74c81a
Axel Lord <axel.lord2000@gmail.com>
2024-10-01 23:18:57 +0200

    added proc macro to generate functions, and tested it with reflink_at

--- async-fs-utils/src/lib.rs
+++ async-fs-utils/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../README.md")]
+#![doc = include_str!("../../README.md")]
 
 use ::std::{
     ffi::{OsStr, OsString},
@@ -11,12 +11,15 @@ use ::std::{
     path::Path,
 };
 
+use ::async_fs_utils_attr::in_blocking;
 use ::nix::{
     dir::Dir,
     errno::Errno,
     fcntl::{AtFlags, OFlag, OpenHow, ResolveFlag},
     sys::stat::{FileStat, Mode},
+    NixPath,
 };
+use ::reflink_at::{OnExists, ReflinkAtError};
 use ::smallvec::SmallVec;
 use ::tokio::task::spawn_blocking;
 use ::tokio::{sync::mpsc::error::SendError, task::JoinError};
@@ -25,9 +28,44 @@ use ::tokio_stream::Stream;
 /// Re-export of nix that matches version used by crate.
 pub use nix;
 
-/// Construct an owned path from a reference.
-fn owned_path<P: AsRef<Path>>(path: P) -> SmallVec<[u8; 32]> {
-    path.as_ref().as_os_str().as_bytes().into()
+/// SmallVec backed path, used for sending paths.
+#[derive(Debug)]
+struct OwnedPath(SmallVec<[u8; 32]>);
+
+impl OwnedPath {
+    /// Create a new owned path from a path-like.
+    pub fn new<P: AsRef<Path>>(path: P) -> Self {
+        Self(path.as_ref().as_os_str().as_bytes().into())
+    }
+}
+
+impl NixPath for OwnedPath {
+    fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn with_nix_path<T, F>(&self, f: F) -> nix::Result<T>
+    where
+        F: FnOnce(&std::ffi::CStr) -> T,
+    {
+        OsStr::from_bytes(&self.0).with_nix_path(f)
+    }
+}
+
+impl AsRef<Path> for OwnedPath {
+    fn as_ref(&self) -> &Path {
+        Path::new(OsStr::from_bytes(&self.0))
+    }
+}
+
+impl From<&Path> for OwnedPath {
+    fn from(value: &Path) -> Self {
+        Self::new(value)
+    }
 }
 
 /// Unwrap a join result by either panic or repanic.
@@ -66,14 +104,8 @@ pub async fn file_stat_at<P>(
 where
     P: AsRef<Path> + Send,
 {
-    let path = owned_path(path);
-    unwrap_joined(
-        spawn_blocking(move || {
-            let path = OsStr::from_bytes(&path);
-            nix::sys::stat::fstatat(fd, path, at_flags)
-        })
-        .await,
-    )
+    let path = OwnedPath::new(path);
+    unwrap_joined(spawn_blocking(move || nix::sys::stat::fstatat(fd, &path, at_flags)).await)
 }
 
 /// Run [nix::dir::Dir::from] in a blocking thread.
@@ -127,13 +159,12 @@ pub async fn open_at_2<P>(
 where
     P: AsRef<Path> + Send,
 {
-    let path = owned_path(path);
+    let path = OwnedPath::new(path);
     unwrap_joined(
         spawn_blocking(move || {
-            let path = OsStr::from_bytes(&path);
             nix::fcntl::openat2(
                 dir_fd,
-                path,
+                &path,
                 OpenHow::new().flags(flags).mode(mode).resolve(resolve),
             )
             .map(|fd| unsafe { OwnedFd::from_raw_fd(fd) })
@@ -155,11 +186,10 @@ pub async fn open_at<P>(
 where
     P: AsRef<Path> + Send,
 {
-    let path = owned_path(path);
+    let path = OwnedPath::new(path);
     unwrap_joined(
         spawn_blocking(move || {
-            let path = OsStr::from_bytes(&path);
-            nix::fcntl::openat(dir_fd, path, flags, mode)
+            nix::fcntl::openat(dir_fd, &path, flags, mode)
                 .map(|fd| unsafe { OwnedFd::from_raw_fd(fd) })
         })
         .await,
@@ -174,14 +204,8 @@ pub async fn read_link_at<P>(dir_fd: Option<RawFd>, path: P) -> Result<OsString,
 where
     P: AsRef<Path> + Send,
 {
-    let path = owned_path(path);
-    unwrap_joined(
-        spawn_blocking(move || {
-            let path = OsStr::from_bytes(&path);
-            nix::fcntl::readlinkat(dir_fd, path)
-        })
-        .await,
-    )
+    let path = OwnedPath::new(path);
+    unwrap_joined(spawn_blocking(move || nix::fcntl::readlinkat(dir_fd, &path)).await)
 }
 
 /// Run [reflink_at::reflink_unlinked] in a blocking thread.
@@ -199,10 +223,9 @@ pub async fn reflink_unlinked<P>(
 where
     P: AsRef<Path> + Send,
 {
-    let path = owned_path(dest);
+    let dest = OwnedPath::new(dest);
     unwrap_joined(
         spawn_blocking(move || {
-            let dest = OsStr::from_bytes(&path);
             reflink_at::reflink_unlinked(
                 dir_fd.map(|dir_fd| unsafe { BorrowedFd::borrow_raw(dir_fd) }),
                 dest.as_ref(),
@@ -226,5 +249,28 @@ pub async fn reflink(dest: RawFd, src: RawFd) -> Result<(), Errno> {
             })
         })
         .await,
+    )
+}
+
+#[in_blocking(wrapped = reflink_at::reflink_at, defer_err)]
+fn reflink_at(
+    /// Directory file descriptor to resolve dest in.
+    dir_fd: Option<RawFd>,
+    /// Where to create reflink.
+    #[path]
+    dest: OwnedPath,
+    /// File to reflink to.
+    src: RawFd,
+    /// What file mode to use for created reflink.
+    mode: Mode,
+    /// How to handle a file existing at dest.
+    on_exists: OnExists,
+) -> Result<OwnedFd, ReflinkAtError> {
+    reflink_at::reflink_at(
+        dir_fd.map(|dir_fd| unsafe { BorrowedFd::borrow_raw(dir_fd) }),
+        dest.as_ref(),
+        unsafe { BorrowedFd::borrow_raw(src) },
+        mode,
+        on_exists,
     )
 }
